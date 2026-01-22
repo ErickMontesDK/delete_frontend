51800d443dfea19ff46ce60607d0ee0e5cff6e14
ErickDKnight <erickmontesdk@gmail.com>
2026-01-21 23:00:26 -0800

    forcing push

--- tasks/views.py
+++ tasks/views.py
@@ -5,3 +5,4 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    # adding changes for..
